34ee14d643bfeebc09fd057bbf098728fc2c8a71
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-29 18:20:55 +0300

    Lesson6_3 10 min + 2 min + 2 min 88% Correct

--- src/Codility/Lesson6_3.java
+++ src/Codility/Lesson6_3.java
@@ -12,7 +12,10 @@ public class Lesson6_3 {
         int indx = A.length - 1;
         int result = A[indx] * A[indx - 1] * A[indx - 2];
         int dresult = A[0] * A[1];
-        if (dresult > A[indx -1] * A[indx -2]) return dresult * A[indx];
+        if (dresult > A[indx -1] * A[indx -2]) {
+            int result2 = dresult * A[indx];
+            if (result2 > result) return  result2;
+        }
         return result;
     }
 }
